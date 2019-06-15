7642f43ba0df84fb47f27f17c8cf36ab9ff6c898
Mike <mjanders6@gmail.com>
2019-06-15 13:22:57 -0700

    update server

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ app.use(express.json())
 
 require('./routes')(app)
 
-var MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost/${process.env.MONGO_DB}`
+// var MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost/${process.env.MONGO_DB}`
 const port = process.env.PORT || 3000
+
+if (port === 3000) {
+    var MONGODB_URI = `mongodb://localhost/${process.env.MONGO_DB}`
+} else {
+    var MONGODB_URI = process.env.MONGODB_URI
+}
 
 require('mongoose').connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
   .then(_ => app.listen(port, () => console.log(`server running on port ${port}`)))
